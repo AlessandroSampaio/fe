2e9f23835fa27edcb3209950c303d31d6fe6d701
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-11-02 19:01:29 -0300

    Adiciona o component Card

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Card,
+  Container, Header, ListContainer,
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -9,6 +15,71 @@ export default function ContactsList() {
         </strong>
         <a href="/">Novo Contato</a>
       </Header>
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={arrow} alt="Ordernar por nome" />
+          </button>
+        </header>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Name</strong>
+              <small>instagram</small>
+            </div>
+            <span>email</span>
+            <span>telefone</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Name</strong>
+              <small>instagram</small>
+            </div>
+            <span>email</span>
+            <span>telefone</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Name</strong>
+              <small>instagram</small>
+            </div>
+            <span>email</span>
+            <span>telefone</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
