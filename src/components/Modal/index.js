25f0b021bec33a0f1a521bfc1c8f70fc7f51b303
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-11-03 11:38:40 -0300

    Aplica ReactPortal ao componente Modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
+import { createPortal } from 'react-dom';
 import { Container, Footer, Overlay } from './styles';
 import Button from '../Button';
 
 export default function Modal({ danger }) {
-  return (
+  return createPortal(
     <Overlay>
       <Container danger={danger}>
         <h1>Titulo do modal</h1>
@@ -13,7 +14,8 @@ export default function Modal({ danger }) {
           <Button type="button" danger={danger}>Deletar</Button>
         </Footer>
       </Container>
-    </Overlay>
+    </Overlay>,
+    document.getElementById('modal-root'),
   );
 }
 
